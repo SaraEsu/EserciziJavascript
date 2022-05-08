77e5d49059ec452d3fe6dae6842a0978b3e06924
Sara Esu <saraesu@gmail.com>
2022-05-08 09:10:37 +0200

    feat: add JSON methods exercises

--- 52 - JSON methods/exercise-4.js
+++ 52 - JSON methods/exercise-4.js
@@ -1,19 +1,15 @@
 const person = {
   id: 1,
-  firstName:'Mario',
-  lastName:'Rossi',
+  firstName: 'Mario',
+  lastName: 'Rossi',
   age: 25,
 };
 
-
-for(values in person){
-if(typeof(values) === "String"){
-  delete person.values;
+for (values in person) {
+  if (typeof (person[values]) === "string") {
+    delete person[values];
+  }
 }
-}
-
-
 
 const json = JSON.stringify(person);
-
 console.log(json); // Should return: { id: 1, age: 25 }
